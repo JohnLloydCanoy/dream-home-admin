34701dd588ed71d76e4e1616327eb57515d9c45e
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-03 22:46:28 +0800

    adding the placeholder

--- src/app/middleware.js
+++
@@ -1,2 +0,0 @@
-// Conditions, if the users are categorize as renter or owner of the property, they will be redirected to the appropriate dashboard, if the user is admin they will be redirected to the admin dashboard, IF THE user is a staff then they will be redirected to the staff dashboard,
-
