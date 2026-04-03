6b4fff65e5b464387ffea783ae0577e1ac63e58c
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-03 16:41:15 +0800

    adding note

---
+++ src/app/middleware.js
@@ -0,0 +1,2 @@
+// Conditions, if the users are categorize as renter or owner of the property, they will be redirected to the appropriate dashboard, if the user is admin they will be redirected to the admin dashboard, IF THE user is a staff then they will be redirected to the staff dashboard,
+
