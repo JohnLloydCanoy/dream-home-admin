88d88f09b663f56f7860d583af6e6ea140b2ffd6
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-04 18:56:41 +0800

    adding buttom

--- src/app/Pages/Admin/Branches/page.js
+++ src/app/Pages/Admin/Branches/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
+import Button from '@/components/ui/Button';
 
 export default function BranchOverviewPage() {
     const [branches, setBranches] = useState([]);
