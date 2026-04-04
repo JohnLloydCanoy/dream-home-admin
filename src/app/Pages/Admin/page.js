d836277b120545b300b337707976abbae08335cb
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-04 11:10:18 +0800

    clearning

--- src/app/Pages/Admin/page.js
+++ src/app/Pages/Admin/page.js
@@ -5,6 +5,9 @@ export default function AdminPage() {
     return (
         <div>
             <ManagementNavBar />
+            <div className="ml-64">
+                <h1>Admin Page</h1>
+            </div>
         </div>
     )
 }
