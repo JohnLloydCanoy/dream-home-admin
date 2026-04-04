d31a4265a64cb01966831e1ad839e7684bc17493
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-04 12:04:49 +0800

    adding a placeholder

--- src/app/Pages/Admin/Staff/Enroll/page.js
+++ src/app/Pages/Admin/Staff/Enroll/page.js
@@ -1,15 +1,16 @@
-import ManagementNavBar from "./components/ui/Navbar";
+import ManagementNavBar from "../../components/ui/Navbar";
 
-export default function EnrollStaffPage() {
+export default function AdminLayout({ children }) {
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-[#002147]">Enroll New Staff</h1>
-      <p className="text-gray-600">Fill out the form below to add a new member to the system.</p>
-      
-      {/* Your Enrollment Form would go here */}
-      <div className="mt-6 bg-white p-6 rounded-xl shadow-sm border border-gray-100">
-          <p className="text-gray-400 italic">Enrollment form coming soon...</p>
-      </div>
+    <div className="flex bg-[#f0f2f5] min-h-screen font-sans">
+
+      <ManagementNavBar />
+
+      <main className="ml-64 flex-1 p-8">
+        <div className="max-w-6xl mx-auto">
+          {children}
+        </div>
+      </main>
     </div>
   );
 }
