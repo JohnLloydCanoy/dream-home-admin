8b76a4f2229c7ba4ff5e0441e4c0988b30b6ba86
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-09 23:38:32 +0800

    feat: update import path for Button component in BranchOverviewPage

--- src/app/Pages/Admin/Branches/page.js
+++ src/app/Pages/Admin/Branches/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import Button from '@components/ui/Button';
+import Button from '../../../../../global-components/ui/Button';
 import apiClient from '@/lib/apiClient';
 import AddBranchModal from '../../../../hooks/editHooks/AddBranchModal';
 import EditBranchModal from '../../../../hooks/editHooks/EditBranchModal';
