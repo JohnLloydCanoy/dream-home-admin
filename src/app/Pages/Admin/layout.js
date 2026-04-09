44139b27791fd5811e70938f7f17ce67edec177b
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-09 21:30:26 +0800

    fix: adjust margin-left for main content in AdminLayout

--- src/app/Pages/Admin/layout.js
+++ src/app/Pages/Admin/layout.js
@@ -5,7 +5,7 @@ export default function AdminLayout({ children }) {
     <div className="flex min-h-screen bg-[#f0f2f5] font-sans">
 
       <ManagementSideBar />
-      <main className="ml-64 flex-1 p-8 overflow-y-auto">
+      <main className="ml-69 flex-1 p-8 overflow-y-auto">
         <div className="max-w-7xl mx-auto">
           {children}
         </div>
