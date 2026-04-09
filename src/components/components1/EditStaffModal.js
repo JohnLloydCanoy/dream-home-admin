ffad1ef2f33e8796091b953b6261b2542ff26086
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-09 23:39:19 +0800

    feat: update import paths for Button and Dialog components in EditStaffModal

--- src/components/components1/EditStaffModal.js
+++ src/components/components1/EditStaffModal.js
@@ -1,8 +1,8 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import Button from '@components/ui/Button';
-import Dialog from '@components/ui/Dialog';
+import Button from '../../../global-components/ui/Button';
+import Dialog from '../../../global-components/ui/Dialog';
 import apiClient from '@/lib/apiClient';
 import useForm from '@/hooks/useForm';
 import { validateForm, staffValidators } from './validator';
