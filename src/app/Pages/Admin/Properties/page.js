7d0fca474d56e2d9d56cbeb3b52b68b7efffaaca
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-05-11 19:44:49 +0800

    refactor: remove unused PropertyFormModal import in PropertiesPage

--- src/app/Pages/Admin/Properties/page.js
+++ src/app/Pages/Admin/Properties/page.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import CrudPageLayout from '@/components/layout/CrudPageLayout';
-import PropertyFormModal from '@/components/ui/PropertyFormModal';
+
 
 export default function PropertiesPage() {
     const tableColumns = [
